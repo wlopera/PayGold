74e4dedf09ae33c16fc109f09261a0a88bb490c7
William Lopera <lopera.william@gmail.com>
2025-06-09 17:35:59 -0500

    version 1

---
+++ dump/backup-9Jun25-17-19/config.js
@@ -0,0 +1,15 @@
+// config.js
+module.exports = {
+  MERCHANT_CODE: "139052351",
+  TERMINAL: "100",
+  TRANSACTION_TYPE: "0",
+  CURRENCY: "978",
+  AMOUNT: "3000",
+  REDSYS_KEY: "sq7HjrUOBfKmC576ILgskD5srU870gJ7",
+  MERCHANT_URL: "https://www.hostalnana.com/",
+  URL_OK: "https://www.hostalnana.com/",
+  URL_KO: "https://www.hostalnana.com/",
+  REDSYS_ENDPOINT: "https://sis-t.redsys.es:25443/sis/realizarPago",
+  SIGNATURE_VERSION: "HMAC_SHA256_V1",
+  LOCAL_TUNNEL_SUBDOMAIN: "mipruebalocal",
+};
